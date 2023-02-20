9dcc34f25eb2ec001d02829f0d9f4f6f9835abf6
jsham <alexisjeongsoo@gmail.com>
2023-02-20 18:12:45 +0900

    20230220 presentational, container 컴포넌트 분리(2)

--- src/components/DisplayNumber.jsx
+++ src/components/DisplayNumber.jsx
@@ -1,20 +1,10 @@
 import React, {Component} from "react";
-import store from "../store";
 export default class DisplayNumber extends Component {
-    state = {number: store.getState().number} // store 의 값을 저장할 지역 변수 (state) 선언 
-
-    // Constructor 를 이용하여 state 가 변경되었음을 통보 받는다.
-    constructor(props) {
-        super(props);  // constructor 이용하려면 반드시 Super method를 호출 해야함. 
-        store.subscribe(function(){ // 구독 하도록 등록  
-            this.setState({number: store.getState().number});
-        }.bind(this));
-    }
     render() {
         return (
             <div>
                 <h1> Display Number</h1>
-                <input type="text" value={this.state.number} readOnly></input>
+                <input type="text" value={this.props.number} readOnly></input>
             </div>
         )
     }
---
+++ src/containers/DisplayNumber.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import DisplayNumber from "../components/DisplayNumber";
+import store from "../store";
+
+export default class extends Component {
+    state = {number: store.getState().number} // store 의 값을 저장할 지역 변수 (state) 선언 
+
+    // Constructor 를 이용하여 state 가 변경되었음을 통보 받는다.
+    constructor(props) {
+        super(props);  // constructor 이용하려면 반드시 Super method를 호출 해야함. 
+        store.subscribe(function(){ // 구독 하도록 등록  
+            this.setState({number: store.getState().number});
+        }.bind(this));
+    }
+    render(){
+        return <DisplayNumber number={this.state.number}></DisplayNumber>
+    }
+}
